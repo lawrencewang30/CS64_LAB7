4fb4b7bd7144590cbf0587ad3b66b7616056fdf7
Lawrence Wang <lawrence_wang@ucsb.edu>
2023-02-21 15:08:11 -0800

    edited conversion.asm

--- conversion.asm
+++ conversion.asm
@@ -8,33 +8,32 @@
     li $t0, 0
     li $t1, 2
     li $t2, 0
-    addu $t3, $a0, $zero
+    add $t3, $a0, $zero
 conv:
     # TODO: Write your function code here
     bgt $t0, 8, exit_loop
-    addiu $t0, $t0, 1
+    addi $t0, $t0, 1
     sll $a0, $a0, 3
-    subu $a2, $a2, $a0
-    addu $a2, $a2, $a1
+    sub $a2, $a2, $a0
+    add $a2, $a2, $a1
 
     slt $t2, $t3, $t1
     beq $t2, $zero, if_statement
 
-    addiu $t3, $t3, 1
-    addu $v0, $a2, $zero
+    addi $t3, $t3, 1
 
     j conv
 
 exit_loop:
+    addi $v0, $a2, 0
     jr $ra
 
 if_statement:
-    addiu $a1, $a1, -1
-    jal conv
+    addi $a1, $a1, -1
 
 main:  # DO NOT MODIFY THE MAIN SECTION
-    li $a0, 21047284
-    li $a1, 124921
+    li $a0, 5
+    li $a1, 7
 
     jal conv
 
